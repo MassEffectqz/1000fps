275f5797353f9cdbf6bdec5c49e5854eea2cfccf
MassEffectqz <hatikohakki@gmail.com>
2026-05-01 15:47:23 +0300

    1

--- prisma/migrations/20260502000000_cart_item_warehouse_unique/migration.sql
+++ prisma/migrations/20260502000000_cart_item_warehouse_unique/migration.sql
@@ -1,6 +1,21 @@
 -- Add warehouseId to unique constraint for CartItem
 -- This allows the same product from different warehouses to be separate cart items
 
-ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_cartId_productId_key";
+-- Try to find and drop existing unique constraint on (cartId, productId)
+DO $$
+DECLARE
+    constraint_name text;
+BEGIN
+    FOR constraint_name IN 
+        SELECT conname 
+        FROM pg_constraint 
+        WHERE conrelid = 'CartItem'::regclass 
+        AND contype = 'u'
+        AND (SELECT count(*) FROM pg_attribute WHERE attrelid = 'CartItem'::regclass AND attnum = ANY(conkey)) = 2
+    LOOP
+        EXECUTE format('ALTER TABLE "CartItem" DROP CONSTRAINT %I', constraint_name);
+    END LOOP;
+END $$;
 
+-- Add new unique constraint including warehouseId
 ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_cartId_productId_warehouseId_key" UNIQUE ("cartId", "productId", "warehouseId");
